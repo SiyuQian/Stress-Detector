aedd67093e21613f70cf4a8dbd3f3ac9364b6f41
Siyu Qian <david.s.qian@gmail.com>
2021-02-17 18:21:16 +1300

    Fix template folder path * Add gitignore file

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, Response, render_template
 from test2 import VideoCamera
 
-app = Flask(__name__, template_folder='D:/Stress-Detector/templates')
+app = Flask(__name__)
 
 
 @app.route('/')
@@ -14,9 +14,9 @@ def gen(test):
         frame= test.get_frame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-       
+
 @app.route("/predict", methods=['POST', 'GET'])
-def predict():  
+def predict():
     return Response(gen(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
